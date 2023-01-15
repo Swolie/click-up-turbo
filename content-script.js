f4f74583cd897a5e2acd597dd2990123a442cb23
Mike Kabbani <Vrishnaakh@gmail.com>
2023-01-15 10:27:33 +0800

    Fixed n key bug, added debug mode, hidden console messages

--- content-script.js
+++ content-script.js
@@ -1,5 +1,9 @@
+const clickUpTurboDebugMode = false;
+
+
 var cuTaskRows = [];
 let currentFocused = 0;
+
 
 
 function getSelectedTask() {
@@ -24,6 +28,13 @@ const turbo_style = `.active-list-item .cu-task-row__main {
 `;
 
 
+function testing(logString) {
+  if (clickUpTurboDebugMode) {
+    console.log(logString);
+  }
+}
+
+
 
 
 
@@ -33,25 +44,40 @@ $(document).ready( function(){
 
 // Begin Jquery Ready
 addNewTaskEventListner();
-console.log("Loaded Clickup Turbo");
+
+testing("Loaded Clickup Turbo");
 
 // Addd Style
 document.head.appendChild(style);
 style.innerHTML = turbo_style;
 
 
+///////////////////
+///////////////////
 function addNewTaskEventListner() {
-  document.addEventListener("keydown", function(event) {
-    if (event.key === "n") {
-        event.preventDefault();
-        event.stopPropagation();
-    }
+///////////////////
+///////////////////
 
-    if (event.key === "n" && !(event.target instanceof HTMLInputElement || event.target instanceof HTMLTextAreaElement)) {
+
+  testing("Loaded Clickup Turbo");
+
+  testing("Added Key Down Event Listener");
+
+
+  document.addEventListener("keydown", function(event) {
+    testing("Keydown Event Listener Triggered");
+
+
+    if (event.key === "n" && !(event.target instanceof HTMLInputElement || event.target instanceof HTMLTextAreaElement) && !(event.target.hasAttribute("contenteditable") && event.target.getAttribute("contenteditable") === "true")) {
+
+      testing("Key N Pressed");
+
       if(cuTaskRows[currentFocused] != null && cuTaskRows[currentFocused] != undefined) {
+          event.preventDefault();
+          event.stopPropagation();
           var newTaskButton = $(getSelectedTask()).parents(".cu-list-group").find("button.cu-task-list-footer__add");
-          console.log("New Task Button");
-          console.log(newTaskButton);
+          testing("New Task Button");
+          testing(newTaskButton);
           $(newTaskButton).click();
         }
     }
@@ -59,58 +85,69 @@ function addNewTaskEventListner() {
 }
 
 
+///////////////////
 function waitingForDOM() {
+///////////////////
+
+  testing("Start waiting for DOM");
+
+
   cuTaskRows = document.querySelectorAll("cu-task-row");
   attempts++;
     // If cutTaskRows is empty, then wait 500ms and try again
     // Make only 20 attempts
 
   if (cuTaskRows.length === 0) {
-    console.log("No task found");
+    testing("No task found");
 
     if (attempts < 20) {
-      console.log("Waiting for DOM — Attempt " + attempts + "");
+      testing("Waiting for DOM — Attempt " + attempts + "");
       timeout = setTimeout(waitingForDOM, 500);
     }
     else {
-      console.log("Stopping");
+      testing("Stopping");
       clearTimeout(timeout);
       return;
     }
   }
   else {
     attempts = 20;
-    console.log("DOM Loaded After " + attempts + " attempts");
-    console.log("Number of Rows = " + cuTaskRows.length);
+    testing("DOM Loaded After " + attempts + " attempts");
+    testing("Number of Rows = " + cuTaskRows.length);
     clearTimeout(timeout);
     domLoadedFunction();
   }
 }
 
 
-
-
-
 // This starts the process
-
 timeout = setTimeout(waitingForDOM, 500);
 
 
-
-
-
+///////////////////
 function domLoadedFunction() {
+///////////////////
+
+  testing("Dom Loaded Function");
 
   // Prevent multiple runs
   if (domLoaded) {
-    console.log("Prevented multiple loading");
+    testing("Prevented multiple loading");
     return;
   }
   domLoaded = true;
 
   var observerWorking = false;
 
-  // Change Observer
+
+
+
+
+////////////////////
+// Change Observer
+////////////////////
+
+
   const observer = new MutationObserver(() => {
     if (observerWorking) {
       return;
@@ -120,49 +157,89 @@ function domLoadedFunction() {
 
     // Add 500ms delay
     setTimeout(function() {
-      console.log("Invoked observer function");
+      testing("Invoked observer function");
       initializeKeyboardShortcuts();
       observerWorking = false;
     }, 1000);
   });
 
 
-  function initializeKeyboardShortcuts() {
-      cuTaskRows = document.querySelectorAll("cu-task-row");
-      console.log(cuTaskRows);
 
-      cuTaskRows.forEach((element, index) => {
-          element.setAttribute("order", index);
-      });
-      document.removeEventListener("keydown", onKeyDown);
-      document.addEventListener("keydown", onKeyDown);
-      cuTaskRows[currentFocused].classList.add("active-list-item");
+
+
+///////////////////
+///////////////////
+  function initializeKeyboardShortcuts() {
+///////////////////
+///////////////////
+
+
+
+    testing("Initialize Keyboard Shortcuts");
+
+
+    cuTaskRows = document.querySelectorAll("cu-task-row");
+    testing(cuTaskRows);
+
+    cuTaskRows.forEach((element, index) => {
+        element.setAttribute("order", index);
+    });
+    document.removeEventListener("keydown", onKeyDown);
+    document.addEventListener("keydown", onKeyDown);
+    cuTaskRows[currentFocused].classList.add("active-list-item");
+
+
+
   }
 
-  function onKeyDown(event) {
-      if (event.code === "KeyK") {
-          currentFocused = currentFocused === 0 ? cuTaskRows.length - 1 : currentFocused - 1;
-          cuTaskRows.forEach((el) => el.classList.remove("active-list-item"));
-          cuTaskRows[currentFocused].classList.add("active-list-item");
-          getSelectedTask().focus();
-          getSelectedTask().scrollIntoView({ behavior: 'smooth', block: 'center' });
 
-      } else if (event.code === "KeyJ") {
-          currentFocused = currentFocused === cuTaskRows.length - 1 ? 0 : currentFocused + 1;
-          cuTaskRows.forEach((el) => el.classList.remove("active-list-item"));
-          cuTaskRows[currentFocused].classList.add("active-list-item");
-          getSelectedTask().focus()
-          getSelectedTask().scrollIntoView({ behavior: 'smooth', block: 'center' });
-      } else if (event.code === "KeyX") {
-          if (cuTaskRows[currentFocused]) {
-              const cuTaskRowStatus = cuTaskRows[currentFocused].querySelector(".cu-task-row-status__done-btn");
-              cuTaskRowStatus.click();
-          }
-      }
+
+
+
+
+
+
+///////////////////
+///////////////////
+  function onKeyDown(event) {
+///////////////////
+///////////////////
+
+    testing("Key Down Event");
+
+
+    // Key K & J (Up & Down)
+    if (event.code === "KeyK" || event.code === "KeyJ") {
+
+        if (event.code === "KeyK") {
+            currentFocused = currentFocused === 0 ? cuTaskRows.length - 1 : currentFocused - 1;
+        } else {
+            currentFocused = currentFocused === cuTaskRows.length - 1 ? 0 : currentFocused + 1;
+        }
+
+        // Remove active class from all rows
+        cuTaskRows.forEach((el) => el.classList.remove("active-list-item"));
+
+        // Add active class to current row
+        cuTaskRows[currentFocused].classList.add("active-list-item");
+
+        // Scroll to current row
+        getSelectedTask().focus()
+        getSelectedTask().scrollIntoView({ behavior: 'smooth', block: 'center' });
+
+
+    // Key X for Toggle Task Completion
+    } else if (event.code === "KeyX") {
+        if (cuTaskRows[currentFocused]) {
+            const cuTaskRowStatus = cuTaskRows[currentFocused].querySelector(".cu-task-row-status__done-btn");
+            cuTaskRowStatus.click();
+        }
+    }
+
   }
 
   initializeKeyboardShortcuts();
-  const targetNode = document.querySelector("body");
+  const targetNode = document.querySelector("cu-task-row");
   const config = {
       childList: true,
       subtree: true
